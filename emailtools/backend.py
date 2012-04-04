8bee7c758286dde6a7ed0bf8f7799bd5bccb93ad
Matthew Tretter <matthew@exanimo.com>
2012-04-04 17:35:21 -0400

    Make it work with non-multipart messages

--- emailtools/backend.py
+++ emailtools/backend.py
@@ -1,5 +1,8 @@
 from django.core.mail.backends.filebased import EmailBackend
 import email
+import mimetypes
+import os
+
 
 class CleanEmailBackend(EmailBackend):
     """ Takes email messages and splits there parts into clean messages """
@@ -22,24 +25,26 @@ class CleanEmailBackend(EmailBackend):
             template and a txt file for a txt template.
     '''
 
+    def _write_file(self, contents, ext):
+        filename, old_ext = os.path.splitext(self._get_filename())
+        output_filename = u'%s%s' % (filename, ext)
+        fp = open(output_filename, 'w')
+        fp.write(contents)
+        fp.close()
+
+    def _write_message(self, message, content_type='text/plain'):
+        if message.is_multipart():
+            for m in message.get_payload():
+                self._write_message(m)
+        else:
+            content_type = message.get_content_type()
+            ext = '.txt' if content_type == 'text/plain' else \
+                    mimetypes.guess_extension(content_type)
+            if ext:
+                self._write_file(message.get_payload(decode=True), ext)
+
     def close(self):
-
         if self.stream is not None:
-            messages = email.message_from_file(open(self._get_filename(), 'r'))
-
-            for message in messages.get_payload():
-                content_type = message.get_content_type()
-
-                if content_type == 'text/html':
-                    body = message.get_payload(decode=True)
-                    file_suffix = 'html'
-
-                elif content_type == 'text/plain':
-                    body = message.as_string()
-                    file_suffix = 'txt'
-
-                output = open(self._get_filename().replace('log', file_suffix), 'w')
-                output.write(body)
-
+            message = email.message_from_file(open(self._get_filename(), 'r'))
+            self._write_message(message)
         super(CleanEmailBackend, self).close()
-
