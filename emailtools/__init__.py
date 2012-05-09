14889ac1615b203be54f095ada4414a3a4c1e37f
Chris McKenzie <Chrismc@hzdg.com>
2012-05-09 10:35:14 -0400

    renaming template and html_template to template_name and html_template_name for consistency with Django's naming

--- emailtools/__init__.py
+++ emailtools/__init__.py
@@ -9,7 +9,7 @@ class BaseTemplatedEmailMessage(EmailMultiAlternatives):
     """
     def __init__(self, subject='', body='', from_email=None, to=None, bcc=None,
             connection=None, attachments=None, headers=None, alternatives=None,
-            cc=None, template=None, html_template=None, extra_context=None):
+            cc=None, template_name=None, html_template_name=None, extra_context=None):
 
         subject = self._get_value('subject', subject)
         body = self._get_value('body', body)
@@ -25,8 +25,8 @@ class BaseTemplatedEmailMessage(EmailMultiAlternatives):
         if cc:
             kwargs['cc'] = cc
 
-        self.template = self._get_value('template', template)
-        self.html_template = self._get_value('html_template', html_template)
+        self.template_name = self._get_value('template_name', template_name)
+        self.html_template_name = self._get_value('html_template_name', html_template_name)
         self.extra_context = self._get_value('extra_context', extra_context)
 
         super(BaseTemplatedEmailMessage, self).__init__(subject=subject, 
@@ -34,27 +34,27 @@ class BaseTemplatedEmailMessage(EmailMultiAlternatives):
                     connection=connection,  attachments=attachments, 
                     headers=headers, alternatives=alternatives, **kwargs)
 
-    def get_templates(self, *args, **kwargs):
+    def get_template_names(self, *args, **kwargs):
         """
         Returns a list of "standard template" names. Must return a list.
         """
-        if self.template is None:
+        if self.template_name is None:
             raise ImproperlyConfigured(
                 "TemplatedEmailMessage requires either a definition of "
                 "'template' or an implementation of 'get_templates()'")
         else:
-            return [self.template]
+            return [self.template_name]
 
-    def get_html_templates(self, *args, **kwargs):
+    def get_html_template_names(self, *args, **kwargs):
         """
         Returns a list of "alternative template" names. Must return a list.
         """
-        if self.html_template is None:
+        if self.html_template_name is None:
             raise ImproperlyConfigured(
                 "TemplatedEmailMessage requires either a definition of "
                 "'html_template' or an implementation of 'get_html_templates()'")
         else:
-            return [self.html_template]
+            return [self.html_template_name]
 
     def get_context_data(self, **kwargs):
         context_data = self.extra_context.copy() if self.extra_context else {}
@@ -66,7 +66,7 @@ class BaseTemplatedEmailMessage(EmailMultiAlternatives):
 
     def send(self, fail_silently=False):
         self.body = self._render_template()
-        if self.html_template:
+        if self.get_html_template_names():
             self.attach_alternative(self._render_html_template(), 'text/html')
 
         return super(BaseTemplatedEmailMessage, self).send(fail_silently)
@@ -75,14 +75,14 @@ class BaseTemplatedEmailMessage(EmailMultiAlternatives):
         """
         Renders standard template with context
         """
-        template = loader.get_template(self.get_templates()[0])
+        template = loader.get_template(self.get_template_names()[0])
         return template.render(self.get_context())
 
     def _render_html_template(self):
         """
         Renders alternative template with context
         """
-        template = loader.get_template(self.get_html_templates()[0])
+        template = loader.get_template(self.get_html_template_names()[0])
         return template.render(self.get_context())
     
     def _get_value(self, attr, value):
