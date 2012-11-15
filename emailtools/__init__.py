74876383f91615e882493d0c03ae8afe02f99e5b
Chris McKenzie <chrismc@hzdg.com>
2012-11-15 10:05:48 -0500

    using versiontools

--- emailtools/__init__.py
+++ emailtools/__init__.py
@@ -1,1 +1,5 @@
 from .templates import BaseTemplatedEmailMessage, TemplatedEmailMessage
+
+__title__ = 'django-emailtools'
+__author__ = 'Chris McKenzie, Matthew Tretter, Colin Wood, Mohammad Taleb, Eric Eldredge'
+__version__ = (0, 3, 0, 'dev', 0)
--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup, find_packages
 # Execute emailtools/version.py to add __version__ to setup.py namespace.
 # This way, we avoid the django imports that are triggered by importing
 # any members of the emailtools module.
-execfile('emailtools/version.py')
 
 setup(
     name='django-emailtools',
-    version='.'.join(map(str, __version__)),
+    version=':versiontools:emailtools:',
     author='Chris McKenzie',
     author_email='chrismc@hzdg.com',
     packages=find_packages(),
@@ -23,5 +22,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
+    ],
+    setup_requires=[
+        'versiontools >= 1.8',
     ]
 )
