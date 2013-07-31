2eaeeaf1373e8c80a7f7e9313b60563bcfc1303b
Matthew Tretter <m@tthewwithanm.com>
2013-07-31 15:22:30 -0400

    Simplify TemplatedEmailMessage

--- mailmate/templates.py
+++ mailmate/templates.py
@@ -3,11 +3,29 @@ from django.core.mail import EmailMultiAlternatives
 from django.template import Context, loader
 
 
-class BaseTemplatedEmailMessage(EmailMultiAlternatives):
+class TemplatedEmailMessage(EmailMultiAlternatives):
     """
+    example:
+    In Emails.py
+
+        from mailmate import TemplateMessage
+
+        class CoolMessage(TemplateMessage):
+            to = ['some-user@some-email.com']
+            from_email = 'no-reply@some-email.com'
+            subject = 'Super Cool Message'
+            template_name = 'emails/text-template.txt'
+            html_template_name = 'emails/html-template.html'
+
+    In Views.py
+
+        from .emails import CoolMessage
+
+        message = CoolMessage(extra_context={'user': 'You', 'is': 'Cool'})
+        message.send()
     """
 
-    def __init__(self, subject='', body='', from_email=None, to=None, bcc=None,
+    def __init__(self, subject='', body=None, from_email=None, to=None, bcc=None,
                  connection=None, attachments=None, headers=None, alternatives=None,
                  cc=None, template_name=None, html_template_name=None,
                  extra_context=None):
@@ -31,7 +49,14 @@ class BaseTemplatedEmailMessage(EmailMultiAlternatives):
                                                   html_template_name)
         self.extra_context = self._get_value('extra_context', extra_context)
 
-        super(BaseTemplatedEmailMessage, self).__init__(
+        if body is None:
+            body = self.render_body()
+
+        alternatives = alternatives or []
+        if self.html_template_name:
+            alternatives.append((self.render_html_body(), 'text/html'))
+
+        super(TemplatedEmailMessage, self).__init__(
             subject=subject,
             body=body, from_email=from_email, to=to, bcc=bcc,
             connection=connection, attachments=attachments,
@@ -44,7 +69,7 @@ class BaseTemplatedEmailMessage(EmailMultiAlternatives):
         if self.template_name is None:
             raise ImproperlyConfigured(
                 "TemplatedEmailMessage requires either a definition of "
-                "'template' or an implementation of 'get_templates()'")
+                "'template' or an implementation of 'get_template_names()'")
         else:
             return [self.template_name]
 
@@ -56,29 +81,20 @@ class BaseTemplatedEmailMessage(EmailMultiAlternatives):
             return [self.html_template_name]
         return None
 
-    def get_context_data(self, **kwargs):
-        context_data = self.extra_context.copy() if self.extra_context else {}
-        context_data.update(kwargs)
-        return context_data
+    def get_context_data(self):
+        return self.extra_context.copy() if self.extra_context else {}
 
-    def get_context(self, context_dict={}):
-        return Context(self.get_context_data(**context_dict))
+    def get_context(self):
+        return Context(self.get_context_data())
 
-    def send(self, fail_silently=False):
-        self.body = self._render_template()
-        if self.get_html_template_names():
-            self.attach_alternative(self._render_html_template(), 'text/html')
-
-        return super(BaseTemplatedEmailMessage, self).send(fail_silently)
-
-    def _render_template(self):
+    def render_body(self):
         """
         Renders standard template with context
         """
         template = loader.get_template(self.get_template_names()[0])
         return template.render(self.get_context())
 
-    def _render_html_template(self):
+    def render_html_body(self):
         """
         Renders alternative template with context
         """
@@ -87,27 +103,3 @@ class BaseTemplatedEmailMessage(EmailMultiAlternatives):
 
     def _get_value(self, attr, value):
         return value or getattr(self.__class__, attr, None) or value
-
-
-class TemplatedEmailMessage(BaseTemplatedEmailMessage):
-    """
-    example:
-    In Emails.py
-
-        from mailmate import TemplateMessage
-
-        class CoolMessage(TemplateMessage):
-            to = ['some-user@some-email.com']
-            from_email = 'no-reply@some-email.com'
-            subject = 'Super Cool Message'
-            template_name = 'emails/text-template.txt'
-            html_template_name = 'emails/html-template.html'
-
-    In Views.py
-
-        from .emails import CoolMessage
-
-        message = CoolMessage(extra_context={'user': 'You', 'is': 'Cool'})
-        message.send()
-    """
-    pass
