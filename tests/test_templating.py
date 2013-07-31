ce6d3f8550d3e26e12774d3561692e21276df73a
Matthew Tretter <m@tthewwithanm.com>
2013-07-31 15:30:51 -0400

    Test HTML body rendering

--- tests/test_templating.py
+++ tests/test_templating.py
@@ -6,3 +6,12 @@ def test_template():
         template_name = 'body.txt'
 
     assert Email(extra_context={'name': 'world'}).body.strip() == 'hello world'
+
+
+def test_html_template():
+    class Email(TemplatedEmailMessage):
+        template_name = 'body.txt'
+        html_template_name = 'body.html'
+
+    email = Email(extra_context={'name': 'world'})
+    assert '<p>hello world</p>' in email.message().as_string()
