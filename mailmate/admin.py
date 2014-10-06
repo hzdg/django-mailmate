9caeef026c638f21de6c4375d5ade8365bc913ee
Colin Wood <cwood06@gmail.com>
2014-10-06 11:29:47 -0400

    Create receiver admin

--- mailmate/admin.py
+++ mailmate/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from .models import Email, Receiver
 
 
-class ReceiverInlineAdmin(admin.TabularInline):
-    model = Receiver
+class ReceiverAdmin(admin.ModelAdmin):
+    pass
 
 
 class EmailAdmin(admin.ModelAdmin):
-    inlines = (ReceiverInlineAdmin, )
     list_display = ('email_name', 'subject', 'from_email')
 
 
 admin.site.register(Email, EmailAdmin)
+admin.site.register(Receiver, ReceiverAdmin)
