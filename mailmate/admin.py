0b28f8429eff4439fcc59767958cc36f1751c0f8
Emeka Nweze <emekan@hzdg.com>
2016-06-02 17:02:00 -0400

    Add to settings DJANGO_MAILMATE_ADMIN to enable/disable Admin

--- mailmate/admin.py
+++ mailmate/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.conf import settings
 from .models import Email, User
 
 
@@ -10,5 +11,12 @@ class EmailAdmin(admin.ModelAdmin):
     list_display = ('email_name', 'subject', 'from_email')
 
 
-admin.site.register(Email, EmailAdmin)
-admin.site.register(User, UserAdmin)
+"""Add to admin if DJANGO_MAILMATE_ADMIN is true in settings file"""
+
+try:
+    setting = settings.DJANGO_MAILMATE_ADMIN
+    if setting is True:
+        admin.site.register(Email, EmailAdmin)
+        admin.site.register(User, UserAdmin)
+except:
+    pass
