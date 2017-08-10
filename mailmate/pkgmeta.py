06ed66b17cb77e9f0030ccb3eda7ccdd3ca407f0
Dan Rodriguez <drodrz@gmail.com>
2017-08-10 11:33:09 -0400

    Bump version to 1.1.1

--- mailmate/pkgmeta.py
+++ mailmate/pkgmeta.py
@@ -1,7 +1,7 @@
 pkgmeta = dict(
     __title__='django-mailmate',
     __author__='Chris McKenzie, Matthew Tretter, Colin Wood, Mohammad Taleb, Eric Eldredge',
-    __version__='1.1.0',
+    __version__='1.1.1',
 )
 
 globals().update(pkgmeta)
